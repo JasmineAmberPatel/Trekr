95a39cb4e8a1a07a793a3278cfd18d5060d8c0f5
Jasmine <jasmine.patel@footasylum.com>
2021-05-07 11:43:34 +0100

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Jasmine Patel on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
